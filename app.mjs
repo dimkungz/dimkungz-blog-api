7a64ba60527d2ed91ac6582d60600c051161834c
dimkungz <dimkungz@gmail.com>
2026-07-24 07:45:27 +0700

    feat: add CORS support and configure dynamic port in Express app; include Vercel configuration file

--- app.mjs
+++ app.mjs
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/profiles",(req,res) =>{
